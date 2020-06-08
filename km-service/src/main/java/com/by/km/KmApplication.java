7f39811372b45dd30a2f5a31ec4194635f856cde
wbytts <wbytts@163.com>
2020-06-08 10:06:55 +0800

    km-service init

---
+++ km-service/src/main/java/com/by/km/KmApplication.java
@@ -0,0 +1,13 @@
+package com.by.km;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmApplication.class, args);
+    }
+
+}
---
+++ km-service/src/test/java/com/by/km/KmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by.km;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
