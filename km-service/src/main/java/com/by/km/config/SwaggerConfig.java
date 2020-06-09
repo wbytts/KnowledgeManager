9b2c62ae33e1290ad3ebf4ab876c892a1c11bd1c
wbytts <wbytts@163.com>
2020-06-09 14:42:58 +0800

    km-service 添加 Swagger2

---
+++ km-service/src/main/java/com/by/km/config/SwaggerConfig.java
@@ -0,0 +1,61 @@
+package com.by.km.config;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import com.google.common.base.Predicate;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfig
+ */
+@Configuration
+@EnableSwagger2 // 启用 Swagger
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        Predicate<RequestHandler> predicate = new Predicate<RequestHandler>() {
+            @Override
+            public boolean apply(RequestHandler input) {
+                Class<?> declaringClass = input.declaringClass();
+                if (declaringClass == BasicErrorController.class) { // 排除
+                    return false;
+                }
+                if(declaringClass.isAnnotationPresent(RestController.class)) { // 被注解的类
+                    return true;
+                }
+                if(input.isAnnotatedWith(ResponseBody.class)) { // 被注解的方法
+                    return true;
+                }
+                return false;
+            }
+        };
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(predicate)
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口")
+                .version("1.0")
+                .build();
+    }
+}
+
--- km-service/src/main/java/com/by/km/controller/HelloController.java
+++ km-service/src/main/java/com/by/km/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.by.km.controller;
 import com.by.km.entity.Demo;
 import com.by.km.service.DemoService;
 import com.by.km.utils.FastJsonUtils;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,10 @@ public class HelloController {
     @Autowired
     private DemoService demoService;
 
+
     @RequestMapping("/hello")
     @ResponseBody
+    @ApiOperation(value="测试接口", notes="测试接口详细描述")
     String hello() {
         List<Demo> demos = demoService.queryAll();
         return FastJsonUtils.obj2json(demos, true);
